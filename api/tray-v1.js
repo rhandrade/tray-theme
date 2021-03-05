d107d729b39da1789e894359efdaea2f4dba1299
Rafael Andrade <rafaelhandrade@outlook.com>
2021-03-04 22:39:46 -0300

    Adds configure function to project

--- api/tray-v1.js
+++ api/tray-v1.js
@@ -1,5 +1,45 @@
 'use strict';
 
-module.exports = class Api{
+const axios = require('axios');
+
+class Api{
+
+    constructor(key, password, themeId){
+        this.key      = key;
+        this.password = password;
+        this.themeId  = themeId;
+        this.headers  = {
+            'Authorization' : `Token token=${this.key}_${this.password}`
+        }
+    }
+
+    checkConfiguration(){
+
+        let config = {
+            url    : `${Api.API_URL}${Api.CHECK_URI}`,
+            method : 'post',
+            headers: this.headers,
+            params :{
+                'theme_id'    : this.themeId,
+                'gem_version' : Api.GEM_VERSION
+            }
+        }
+
+        return axios.request(config)
+            .then((response) => response.data)
+            .catch((error) => error.response.data);
+    }
+
 
 }
+
+Api.GEM_VERSION      = '1.0.4';
+Api.API_URL          = 'https://opencode.tray.com.br/api/';
+Api.CLEAN_CACHE_URI  = 'clean_cache';
+Api.CHECK_URI        = 'check'
+Api.LIST_URI         = 'list';
+Api.THEME_URI        = 'themes'
+Api.THEME_ASSETS_URI = 'themes/{theme-id}/assets'
+
+
+module.exports = Api;
--- bin/cli.js
+++ bin/cli.js
@@ -1,12 +1,94 @@
 #!/usr/bin/env node
 
-const packageConfig = require('../package.json');
 const { program }   = require('commander');
 const fs            = require('fs');
 const chalk         = require('chalk');
 const chokidar      = require('chokidar');
 const inquirer      = require('inquirer');
 const axios         = require('axios');
+const yaml          = require('js-yaml');
+
+const packageConfig = require('../package.json');
+const Api           = require('../api/tray-v1');
+
+
+program
+    .command('configure')
+    .arguments('[key] [password] [theme_id]')
+    .description('Create configuration file', {
+        key      : 'Api key',
+        password : 'Api password',
+        theme_id : 'Theme id'
+    })
+    .action(async (key, password, theme_id) => {
+
+        let questions = [];
+        let answers = {
+            key      : key,
+            password : password,
+            theme_id : theme_id,
+        };
+
+        if(!answers.key){
+            questions.push(
+                {
+                    type    : 'input',
+                    message : 'Enter api key',
+                    name    : 'key'
+                }
+            );
+        }
+
+        if(!answers.password){
+            questions.push(
+                {
+                    type    : 'input',
+                    message : 'Enter api password',
+                    name    : 'password'
+                }
+            );
+        }
+
+        if(!answers.theme_id){
+            questions.push(
+                {
+                    type    : 'input',
+                    message : 'Enter theme id',
+                    name    : 'theme_id'
+                }
+            );
+        }
+
+        if(questions.length > 0){
+            let missingAnswers = await inquirer.prompt(questions);
+            answers = { ...answers, ...missingAnswers};
+        }
+
+        let api = new Api(answers.key, answers.password, answers.theme_id);
+        let response = await api.checkConfiguration();
+
+        if(!response.authentication){
+            console.log(chalk`{red [Fail]} Api key, password or theme id not correctly. Please verify and tray again.`);
+            process.exit();
+        }
+
+        let configFileData = yaml.dump({
+
+            api_key     : answers.key,
+            password    : answers.password,
+            theme_id    : answers.theme_id,
+            preview_url : response.preview
+
+        }, { forceQuotes : true });
+
+        fs.writeFileSync('config.yml', configFileData, 'utf8');
+
+
+    });
+
+
+
+
 
 
 program.version(packageConfig.version)
