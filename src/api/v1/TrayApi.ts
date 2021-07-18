3c89a33bc36b0f5785e986b9d10804d934320a4c
Rafael Andrade <rafaelhandrade@outlook.com>
2021-07-18 16:44:40 -0300

    Migrates project from JavaScript to TypeScript.
    Migrate project to use Typescript instead js;
    Adds eslint config to improve code writing and validation;
    Adds prettier config to automate file format following eslint config;
    Adds editorconfig file to sync major code patterns followed by project;

---
+++ src/api/v1/TrayApi.ts
@@ -0,0 +1,252 @@
+import axios, { AxiosRequestConfig } from "axios";
+import FileType from "file-type";
+
+interface ITrayApi {
+    key: string;
+    password: string;
+    themeId?: number | null;
+}
+
+class TrayApi {
+    readonly GEM_VERSION = "1.0.4";
+    readonly API_URL = "https://opencode.tray.com.br/api";
+
+    key: string;
+    password: string;
+    themeId: number | null;
+    headers: object;
+
+    /**
+     * Initiate new TrayApi class
+     * @param param0
+     */
+    constructor({ key, password, themeId = null }: ITrayApi) {
+        this.key = key;
+        this.password = password;
+        this.themeId = themeId;
+        this.headers = {
+            Authorization: `Token token=${this.key}_${this.password}`,
+            Accept: "application/json",
+        };
+    }
+
+    /**
+     * Check configurations files
+     * @returns Object with previewUrl in success case, or error message otherwise.
+     */
+    checkConfiguration() {
+        const config: AxiosRequestConfig = {
+            url: `${this.API_URL}/check`,
+            method: "post",
+            headers: this.headers,
+            params: {
+                gem_version: this.GEM_VERSION,
+            },
+        };
+
+        if (this.themeId) {
+            config.params.theme_id = this.themeId;
+        }
+
+        return axios
+            .request(config)
+            .then((response) => ({
+                success: true,
+                previewUrl: this.themeId ? response.data.preview : null,
+            }))
+            .catch((error) => ({
+                success: false,
+                message: error.response.data.message,
+            }));
+    }
+
+    /**
+     * Get a list of all themes available at store
+     * @returns Object with themes list in success case, or error message otherwise.
+     */
+    getThemes() {
+        const config: AxiosRequestConfig = {
+            url: `${this.API_URL}/list`,
+            method: "get",
+            headers: this.headers,
+            params: {
+                gem_version: this.GEM_VERSION,
+            },
+        };
+
+        return axios
+            .request(config)
+            .then((response) => ({
+                success: true,
+                themes: response.data.themes,
+            }))
+            .catch((error) => ({
+                success: false,
+                message: error.response.data.message,
+            }));
+    }
+
+    /**
+     * Create a new theme on store.
+     * @param name Name of the new theme
+     * @param themeBase Name of the base theme
+     * @returns Object with theme id and preview url for created theme or error message otherwise.
+     */
+    createTheme(name: string, themeBase: string = "default") {
+        const config: AxiosRequestConfig = {
+            url: `${this.API_URL}/themes`,
+            method: "post",
+            headers: this.headers,
+            params: {
+                gem_version: this.GEM_VERSION,
+            },
+            data: {
+                theme: {
+                    name,
+                    theme_base: themeBase,
+                    gem_version: this.GEM_VERSION,
+                },
+            },
+        };
+
+        return axios
+            .request(config)
+            .then(({ data }) => ({
+                success: true,
+                themeId: data.theme_id,
+                previewUrl: data.preview,
+            }))
+            .catch(({ response }) => ({
+                success: false,
+                message: response.data.message,
+            }));
+    }
+
+    /**
+     * Clean cache for a theme on store
+     * @param themeId
+     * @returns Object with success true in case of success or error message otherwise.
+     */
+    cleanCache(themeId = this.themeId) {
+        const config: AxiosRequestConfig = {
+            url: `${this.API_URL}/clean_cache/`,
+            method: "post",
+            headers: this.headers,
+            params: {
+                theme_id: themeId,
+                gem_version: this.GEM_VERSION,
+            },
+        };
+
+        return axios
+            .request(config)
+            .then((response) => {
+                if (response.data.response.code !== 200) {
+                    return {
+                        success: false,
+                        message: `Unknown error. Details: ${response.data.response.message}`,
+                    };
+                }
+
+                return {
+                    success: true,
+                    message: response.data.response.message,
+                };
+            })
+            .catch((error) => ({
+                success: false,
+                message: error.response.data.message,
+            }));
+    }
+
+    /**
+     * Delete a theme from store
+     * @param themeId Id of theme to delete
+     * @returns Object with success true in case of success or error message otherwise.
+     */
+    deleteTheme(themeId: number) {
+        const config: AxiosRequestConfig = {
+            url: `${this.API_URL}/themes/${themeId}`,
+            method: "delete",
+            headers: this.headers,
+            params: {
+                gem_version: this.GEM_VERSION,
+            },
+        };
+
+        return axios
+            .request(config)
+            .then(() => ({ success: true }))
+            .catch((error) => ({
+                success: false,
+                message: error.response.data.message,
+            }));
+    }
+
+    /**
+     * Get theme assets
+     * @returns Object with assets and its total, error message otherwise.
+     */
+    getThemeAssets() {
+        const config: AxiosRequestConfig = {
+            url: `${this.API_URL}/themes/${this.themeId}/assets`,
+            method: "get",
+            headers: this.headers,
+            params: {
+                gem_version: this.GEM_VERSION,
+            },
+        };
+
+        return axios
+            .request(config)
+            .then((response) => ({
+                success: true,
+                quantity: response.data.meta.total,
+                assets: response.data.assets,
+            }))
+            .catch((error) => ({
+                success: false,
+                message: error.response.data.message ?? error.response.data.error,
+            }));
+    }
+
+    /**
+     * Get theme asset content
+     * @returns Object with asset properties and content or error message otherwise.
+     */
+    getThemeAsset(asset: string) {
+        const config: AxiosRequestConfig = {
+            url: `${this.API_URL}/themes/${this.themeId}/assets`,
+            method: "get",
+            headers: this.headers,
+            params: {
+                key: asset,
+                gem_version: this.GEM_VERSION,
+            },
+        };
+
+        return axios
+            .request(config)
+            .then(async (response) => {
+                const assetContentBuffer = Buffer.from(response.data.content, "base64");
+                const fileType = await FileType.fromBuffer(assetContentBuffer);
+
+                return {
+                    success: true,
+                    asset: {
+                        key: response.data.key,
+                        path: `.${response.data.key}`,
+                        dynamic: Boolean(Number(response.data.dynamic)),
+                        binary: !!fileType,
+                        content: assetContentBuffer,
+                    },
+                };
+            })
+            .catch((error) => ({
+                success: false,
+                message: error.response.data.message ?? error.response.data.error,
+            }));
+    }
+}
+
+export { TrayApi };
---
+++ src/libs/utils.ts
@@ -0,0 +1,62 @@
+import { readFile, writeFile } from "fs/promises";
+import yaml from "js-yaml";
+
+interface IConfigFile {
+    key: string;
+    password: string;
+    themeId: number;
+    previewUrl: string;
+}
+
+interface IConfigFileLoaded {
+    api_key: string;
+    password: string;
+    theme_id: number;
+    preview_url: string;
+}
+
+export function saveConfigFile({ key, password, themeId, previewUrl }: IConfigFile) {
+    const fileDataAsObject = {
+        api_key: key,
+        theme_id: themeId,
+        preview_url: previewUrl,
+        password,
+    };
+
+    const configFileData = yaml.dump(fileDataAsObject, {
+        forceQuotes: true,
+    });
+
+    return writeFile("config.yml", configFileData)
+        .then(() => ({
+            success: true,
+            message: "Configuration file created with success.",
+        }))
+        .catch((error) => ({
+            success: false,
+            message: `Unable to create config file. ${error}`,
+        }));
+}
+
+export function loadConfigFile() {
+    return readFile("config.yml", { encoding: "utf8" })
+        .then((data) => {
+            const config = yaml.load(data) as IConfigFileLoaded;
+
+            const { api_key: key, password, theme_id: themeId, preview_url: previewUrl } = config;
+
+            return {
+                success: true,
+                config: {
+                    key,
+                    password,
+                    themeId,
+                    previewUrl,
+                },
+            };
+        })
+        .catch((error) => ({
+            success: false,
+            message: `Unable to load config file. ${error}`,
+        }));
+}
